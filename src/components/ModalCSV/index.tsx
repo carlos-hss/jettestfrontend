1f97facb32b9fa485d7a37f8322babcb1c349684
Carlos Henrique <carloshenriquesantanas19@gmail.com>
2023-02-16 09:59:16 -0300

    fix: add rereender when upload file

--- src/components/ModalCSV/index.tsx
+++ src/components/ModalCSV/index.tsx
@@ -4,8 +4,12 @@ import { Container, Modal } from "./styles";
 import { IoMdClose } from "react-icons/io";
 import { SecondaryButton } from "../MainContent/styles";
 import { api } from "../../utils/api";
+import { Context } from "../../context";
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
 
 export default function ModalCSV({ setIsModalCSVOpen }: IModalCSVProps) {
+  const { setOperators } = React.useContext(Context);
+  const [isLoading, setIsLoading] = React.useState<boolean>(false);
   const [file, setFile] = React.useState<File>();
 
   const handleClose = () => {
@@ -27,23 +31,28 @@ export default function ModalCSV({ setIsModalCSVOpen }: IModalCSVProps) {
     reader.readAsText(file, "UTF-8");
 
     reader.onload = async () => {
+      setIsLoading(true);
       const csv = reader.result as string;
 
-      try {
-        const response = await api.post("/files", csv, {
-          headers: {
-            "Content-Type": "text/csv",
-          },
-        });
+        try {
+          const response = await api.post("/files", csv, {
+            headers: {
+              "Content-Type": "text/csv",
+            },
+          });
 
-        if (!response) {
-          throw new Error("Failed to upload file");
+          if (!response) {
+            throw new Error("Failed to upload file");
+          }
+
+          const operators = await api.get("/operators");
+
+          setOperators(operators.data);
+
+          setIsLoading(false);
+        } catch (error) {
+          console.error(error);
         }
-
-        console.log(response.data);
-      } catch (error) {
-        console.error(error);
-      }
     };
   };
 
@@ -57,7 +66,7 @@ export default function ModalCSV({ setIsModalCSVOpen }: IModalCSVProps) {
         <div className="content">
           <input type="file" accept=".csv" onChange={handleFileChange} />
           <SecondaryButton onClick={() => handleUploadClick(file)}>
-            Upload
+            {isLoading ? <AiOutlineLoading3Quarters size={13} /> : "Upload"}
           </SecondaryButton>
         </div>
       </Modal>
